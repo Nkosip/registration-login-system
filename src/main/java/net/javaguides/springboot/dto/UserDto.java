866b1e6336f86b9a0b43c3365b39a6e8d4ce3ee4
Nkosip <nkosip1999@gmail.com>
2023-11-08 01:07:53 +0200

    #code

---
+++ src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+    @NotEmpty(message = "first name should not be empty")
+    private String firstName;
+
+    @NotEmpty(message = "last name should not be empty")
+    private String lastName;
+
+    @NotEmpty(message = "email should not be empty")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "password should not be empty")
+    private String password;
+}
